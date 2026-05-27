5f76b26f2ae8e91ab31c58ab99ec2457aca5cb2e
Mary Minasyan <maryminasyan@g.ucla.edu>
2026-05-27 14:08:28 -0700

    fix formatting of constants/line_info + adj Paschen-alpha + add Paschen-beta

--- src/daria/utils/constants.py
+++ src/daria/utils/constants.py
@@ -1,1 +1,18 @@
-# Collection of various constantsimport astropy.constants as constimport astropy.units as uc = float(const.c.to(u.cm/u.s).value)cm_per_mpc = u.Mpc.to(u.cm)m_per_mpc = u.Mpc.to(u.m)erg_per_s_per_nW = u.nW.to(u.erg/u.s)sqdeg_per_std = u.sr.to(u.deg**2)lsun = float(const.L_sun.to(u.erg/u.s).value)r_ab = 2.86                       # Balmer decrement in absence of dust
+import astropy.constants as const
+import astropy.units as u
+
+c = float(const.c.to(u.cm/u.s).value)
+cm_per_mpc = u.Mpc.to(u.cm)
+m_per_mpc = u.Mpc.to(u.m)
+erg_per_s_per_nW = u.nW.to(u.erg/u.s)
+sqdeg_per_std = u.sr.to(u.deg**2)
+
+lsun = float(const.L_sun.to(u.erg/u.s).value)
+r_ab = 2.86 # Balmer decrement in absence of dust
+r_HaPab = 17.6
+r_PaHb = 0.33
+r_PaHa = r_PaHb / r_ab
+
+# Ha L-SFR calibrations
+lsfr_kennicutt98 = 1.27e41 # Kennicutt 1998
+lsfr_murphy11 = 1.862e41 # Murphy et al 2011 / Kennicutt and Evans 2012
--- src/daria/utils/line_info.py
+++ src/daria/utils/line_info.py
@@ -1,1 +1,44 @@
-from .constants import lsun, r_ab# Keep OIII out to avoid double counting constiuent 5007 and 4959 linesline_names = 'Ha', 'Hb', 'Hg', 'Hd', 'He', 'OIII_5007', 'OIII_4959', 'OII', \    'Lya', 'Pa', 'PAH', 'NII'l_a = 1.27e41# A few dicts for convenienceline_lpersfr = \    {     'Lya': 1.21e42,     'Ha': 1.27e41,     'OIII': 1.32e41,              # Assumed to be 5007+4959??     'Hb': l_a / r_ab,     'OIII_5007': 0.75 * 1.32e41,     'OIII_4959': 0.25 * 1.32e41,     'Hg': 0.468*0.44e41,     'Hd': 0.259 * 0.44e41,     'He': 0.159 * 0.44e41,     'OII': 0.71e41,     'Pa': 1.27e41 * 0.123,     'PAH': lsun * 10**6.6,        # 3.3 micron PAH (Lai+ 2020)     'NII': l_a * 0.25,            # Rough number!    }line_rwaves = \    {     'Lya': 1216,     'Ha': 6563,     'Hb': 4861,     'Hg': 4340,     'Hd': 4102,     'He': 3970,     'OII': 3727,     'OIII_5007': 5007,     'OIII_4959': 4959,     'Pa': 1.87e4,     'PAH': 3.3e4,     'NII': 6584,    }line_labels = \    {     'Ha': r'H$\alpha$',     'Hb': r'H$\beta$',     'Hg': r'H$\gamma$'    }
+from .constants import lsun, r_ab, r_PaHa, r_HaPab, lsfr_kennicutt98
+
+# Avoid double-counting OIII
+line_names = ('Lya','Ha','Hb','Hg','Hd','He','OIII_5007','OIII_4959','OII',\
+              'Pa','PAH','NII','Pab')
+
+line_lpersfr = \
+    {
+        'Lya': 1.21e42,
+        'Ha': lsfr_kennicutt98, # 1.27e41
+        'Hb': lsfr_kennicutt98 / r_ab,
+        'Hg': 0.468 * 0.44e41,
+        'Hd': 0.259 * 0.44e41,
+        'He': 0.159 * 0.44e41,
+        'OIII': 1.32e41,
+        'OIII_5007': 0.75 * 1.32e41,
+        'OIII_4959': 0.25 * 1.32e41,
+        'OII': 0.71e41,
+        # 'Pa': 2.174e40, # (Neufeld+2024 section 3.3) 0.123 * lsfr_kennicutt98
+        'Pa': lsfr_kennicutt98 * r_PaHa,
+        'PAH': lsun * 10**6.6, # 3.3 um (Lai+ 2020)
+        'NII': lsfr_kennicutt98 * 0.25,
+        'Pab': lsfr_kennicutt98 / r_HaPab
+    }
+
+line_rwaves = \
+    {
+        'Lya': 1216,
+        'Ha': 6563,
+        'Hb': 4861,
+        'Hg': 4340,
+        'Hd': 4102,
+        'He': 3970,
+        'OII': 3727,
+        'OIII_5007': 5007,
+        'OIII_4959': 4959,
+        'Pa': 1.87e4,
+        'PAH': 3.3e4,
+        'NII': 6584,
+        'Pab': 1.28e4
+    }        
+        
+        
+                
